54783e4f4ded386577e4a0720e5ddc338b420211
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-05-23 15:51:07 +0100

    create a get method for categories depends on the language the the client is requisting

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,25 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateCategorieDto } from './dto/create-categorie.dto';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
-import { Categorie, CategorieDocument } from 'src/schemas/category.schema';
+import { Category, CategorieDocument } from 'src/schemas/category.schema';
 
 
 @Injectable()
 export class CategorieService {
   constructor(
-    @InjectModel(Categorie.name) private categorieModel: Model<CategorieDocument>,
+    @InjectModel(Category.name) private categorieModel: Model<CategorieDocument>,
     private readonly cloudinaryService: CloudinaryService,
   ) {}
 
-  async create(createCategoryDto: CreateCategoryDto): Promise<Category> {
+  async create(createCategoryDto: createCategoryDto): Promise<Category> {
+
     const { category_code, image, ar, fr, en, type_service, status, added_date } = createCategoryDto;
-
-    // Upload image to Cloudinary
     const uploadedImage = await this.cloudinaryService.uploadImage(image, 'Categories');
-
     const createdCategory = new this.categoryModel({
       category_code,
       image: uploadedImage.secure_url,
@@ -34,7 +32,23 @@ export class CategorieService {
     return createdCategory.save();
   }
 
-  async findAll(): Promise<Categorie[]> {
-    return this.categorieModel.find().exec();
+  async findAll(lang: string = 'en'): Promise<any[]> {
+    // let's set a default language 
+    const lang = req.query.lang || 'en';
+
+    const categories = await this.categoryModel.find().exec();
+
+    return categories.map(category => {
+      return {
+        _id: category._id,
+        category_code: category.category_code,
+        image: category.image,
+        // we should return one object of name depends on the language that the client is requesting
+        name: category[lang].name,
+        type_service: category.type_service,
+        status: category.status,
+        added_date: category.added_date,
+      };
+    });
   }
 }
