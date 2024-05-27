0efe7036495b53454f2fc00bd160f7be915e1fe4
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-05-27 11:13:31 +0100

    set a post request in the transactions controller

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { TransactionsService } from '../services/transactions.service';
+import { CreateTransactionDto } from '../dto/create-transaction.dto';
+import { Transaction } from '../schemas/transaction.schema';
 
 @Controller('transactions')
-export class TransactionsController {}
+export class TransactionsController {
+  constructor(private readonly transactionsService: TransactionsService) {}
+
+  @Post()
+  async create(@Body() createTransactionDto: CreateTransactionDto): Promise<Transaction> {
+    return this.transactionsService.create(createTransactionDto);
+  }
+
+  
+}
