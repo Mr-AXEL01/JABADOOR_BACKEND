43f641f238a7f8823ecee81efe0712edae74f7ad
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-05-27 14:32:42 +0100

    set post request to create a resservations

--- src/reservations/reservations.controller.ts
+++ src/reservations/reservations.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { ReservationsService } from '../services/reservations.service';
+import { CreateReservationDto } from '../dto/create-reservation.dto';
+import { Reservation } from '../schemas/reservation.schema';
 
 @Controller('reservations')
-export class ReservationsController {}
+export class ReservationsController {
+  constructor(private readonly reservationsService: ReservationsService) {}
+
+  @Post()
+  async create(@Body() createReservationDto: CreateReservationDto): Promise<Reservation> {
+    return this.reservationsService.create(createReservationDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Reservation[]> {
+    return this.reservationsService.findAll();
+  }
+
+}
