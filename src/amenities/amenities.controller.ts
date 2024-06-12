3ba9bb2bc53c2260f87c1c7ed3dfe2e39265e1f4
zakaria loulida <zakarialoulida92@gmail.com>
2024-06-12 17:27:44 +0100

    les  logelements et les address sont faites

--- src/amenities/amenities.controller.ts
+++ src/amenities/amenities.controller.ts
@@ -1,14 +1,19 @@
 import { Controller, Post, Get, Query, Body } from '@nestjs/common';
 import { AmenityService } from './amenities.service';
-import { CreateAmenityDto } from './dto/create-amenity.dto';
+import { AmenityDto, CreateAmenitiesDto } from './dto/create-amenity.dto';
 
 @Controller('amenities')
 export class AmenityController {
   constructor(private readonly amenityService: AmenityService) {}
 
   @Post()
-  async create(@Body() createAmenityDto: CreateAmenityDto) {
+  async create(@Body() createAmenityDto: AmenityDto) {
     return this.amenityService.create(createAmenityDto);
+  }
+
+  @Post('bulk')
+  async createMany(@Body() createAmenitiesDto: CreateAmenitiesDto) {
+    return this.amenityService.createMany(createAmenitiesDto);
   }
 
   @Get()
